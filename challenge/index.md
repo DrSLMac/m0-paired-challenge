1c45555c8b2374764b3b77d13d74a37cf69227f0
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-04-18 21:37:46 -0500

    Initial commit

---
+++ challenge/index.md
@@ -0,0 +1,11 @@
+# H1 Book Titles
+
+1. Money for the Rest of Us
+2. The Simple Path
+3. Atomic Habits
+4. The Husband's Secret
+5. Apples Never Fall
+
+
+This was really done more individually while consulting each other as needed.
+We both worked well but I was slower, just remembering the commands and trying to keep from getting confused on what I am doing.
---
+++ challenge/pairedChallenge.js
@@ -0,0 +1,8 @@
+//the file is named for the challenge of working in pairs
+var favGenre = "Historical fiction"
+var avgLength = 250
+var favBook = "To Kill a Mockingbird" //this has been my favorite book of all time
+var numBooksRead = 12
+var enjoysReading = true //i thoroughly enjoy time ot read
+var readsDaily = false //though i would like to read daily, it doesn't happen
+var bookGenre = ["Horror", "Fantasy", "Romance"]

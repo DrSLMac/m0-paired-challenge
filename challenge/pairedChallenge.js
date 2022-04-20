d12287b9d4909831fcb168f4e35dd6099eafb3f3
Shauna MacFarlane <shauna.macfarlane579@gmail.com>
2022-04-20 10:41:01 -0500

    Initial commit

--- challenge/pairedChallenge.js
+++ challenge/pairedChallenge.js
@@ -1,8 +1,9 @@
-//the file is named for the challenge of working in pairs
-var favGenre = "Historical fiction"
-var avgLength = 250
-var favBook = "To Kill a Mockingbird" //this has been my favorite book of all time
-var numBooksRead = 12
-var enjoysReading = true //i thoroughly enjoy time ot read
-var readsDaily = false //though i would like to read daily, it doesn't happen
-var bookGenre = ["Horror", "Fantasy", "Romance"]
+//The name of the file is a simple description of why the file was created.
+//Perhaps a better name would have been booksGalore.js
+var favBook = "Wuthering Heights" //I have read this book more times than I can count
+var booksPerMonth = 2
+var avgPages = 200
+var enjoysReading = true //I love learning from a non-fiction book, but then escaping into another world of a fiction
+var lastBookRead = "Money for the Rest of Us" //Never stop learning to be more financially savvy
+var readsDaily = false //I wish I had time to ready daily, but alas. 
+var upNext = ["Can't Hurt Me", "Under the Whispering Door", "Apples Never Fall", "The Four Winds"]
